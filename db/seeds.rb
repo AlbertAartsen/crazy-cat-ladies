bc5eb6d2e8ec139e7fafea735f9ae0f7e578a918
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-07 11:34:04 +0200

    lady seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+lady1 = CrazyCatLady.create!({name: "Christa", age: 52, image_url: "", smell: "smelly"})
+lady2 = CrazyCatLady.create!({name: "Rohana", age: 33, image_url: "", smell: "smelly"})
+lady3 = CrazyCatLady.create!({name: "Debby", age: 89, image_url: "", smell: "smelly"})
+lady4 = CrazyCatLady.create!({name: "Shenaynay", age: 64, image_url: "", smell: "smelly"})
+lady5 = CrazyCatLady.create!({name: "Agatha", age: 65, image_url: "", smell: "smelly"})

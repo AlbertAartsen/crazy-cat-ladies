12b25fa8cdbd66cbdf9711caaaa5a62d7610e199
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-07 15:02:13 +0200

    bla

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :cats
+  resources :cats
     root "crazycatladies#index"
   #
-  # resources :crazycatladies
-    get "crazycatladies" => "crazycatladies#index"
+  resources :crazycatladies
+    
 end
